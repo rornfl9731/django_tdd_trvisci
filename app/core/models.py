62ccbe6e5c6b234398eba11b9d478f5d80194035
jinwoo <inx0930@gmail.com>
2019-08-13 03:28:54 +0900

    아마도 원인을 찾을듯함

--- app/core/models.py
+++ app/core/models.py
@@ -5,36 +5,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-    # def create_user(self, email, password=None, **extra_fields):
-    #     """Creates and saves a new User"""
-    #     if not email:
-    #         raise ValueError('유저는 이메일을 필요로 합니다.')
-    #     user = self.model(email=self.normalize_email(email), **extra_fields)
-    #     user.set_password(password)
-    #     user.save(using=self._db)
-    #
-    #     return user
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         if not email:
-            raise ValueError('Users must have an email address')
-
+            raise ValueError('유저는 이메일을 필요로 합니다.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    # def create_superuser(self, email, password):
-    #     """슈퍼유저 생성 및 저장"""
-    #     user = self.create_user(email, password)
-    #     user.is_staff = True
-    #     user.is_superuser = True
-    #     user.save(using=self._db)
-    #
-    #     return user
     def create_superuser(self, email, password):
-        """Creates and saves a new super user"""
+        """슈퍼유저 생성 및 저장"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
--- app/core/tests/test_modules.py
+++ app/core/tests/test_modules.py
@@ -5,9 +5,9 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
-        password = 'Password123'
+        """이메일로 새로운 유저 생성 테스트"""
+        email = 'test@naver.com'
+        password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -16,43 +16,23 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    # def test_new_user_email_normalized(self):
-    #     """새로운 유저의 이메일 대소문자 테스트"""
-    #     email = "test@NAVER.COM"
-    #     user = get_user_model().objects.create_user(email, 'test123')
-    #
-    #     self.assertEqual(user.email, email.lower())
     def test_new_user_email_normalized(self):
-        """Test the email for a new user is normalized"""
-        email = 'test@LONDONAPPDEV.com'
+        """새로운 유저의 이메일 대소문자 테스트"""
+        email = "test@NAVER.COM"
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
-    # def test_new_user_invalid_email(self):
-    #     """이메일없는 유저 생성할때 에러발생 테스트"""
-    #     with self.assertRaises(ValueError):
-    #         get_user_model().objects.create_user(None, 'test123')
-
     def test_new_user_invalid_email(self):
-        """Test creating user with no email raises error"""
+        """이메일없는 유저 생성할때 에러발생 테스트"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
 
-    # def test_create_new_superuser(self):
-    #     """슈퍼유저 생성 테스트"""
-    #     user = get_user_model().objects.create_superuser(
-    #         "test@naver.com",
-    #         'rlawlsdn1'
-    #     )
-    #     self.assertTrue(user.is_superuser)
-    #     self.assertTrue(user.is_staff)
-    def test_new_superuser(self):
-        """Test creating a new superuser"""
+    def test_create_new_superuser(self):
+        """슈퍼유저 생성 테스트"""
         user = get_user_model().objects.create_superuser(
-            'test@londonappdev.com',
-            'test123'
+            "test@naver.com",
+            'rlawlsdn1'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
